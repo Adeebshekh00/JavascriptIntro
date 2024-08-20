34646a6545c9e4c0e48109600734fa264f6e3a6d
Adeebshekh00 <127036395+Adeebshekh00@users.noreply.github.com>
2024-08-20 20:10:37 +0000

    additions to conversion

--- JsBasics/conversion.js
+++ JsBasics/conversion.js
@@ -4,4 +4,8 @@ console.log(typeof(score));
 let isLoggedIn = 1;
 
 let booleanval = Boolean(isLoggedIn);
-console.log(booleanval);
+//console.log(booleanval);
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
