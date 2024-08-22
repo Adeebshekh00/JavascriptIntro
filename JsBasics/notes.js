48afabf7fd9638d67f45ebf15c2dce8c45a0eb16
Adeebshekh00 <127036395+Adeebshekh00@users.noreply.github.com>
2024-08-22 08:47:03 +0000

    notes

---
+++ JsBasics/notes.js
@@ -0,0 +1,40 @@
+//stack(primitive) -> copy me change hoga .
+
+// heap(non-primitive)-> reference me change hoga.
+
+// To generate random values in a range 
+
+//const min = 10
+//const max = 20
+
+//console.log(Math.floor(Math.random()*(max-min+1) + min))
+
+
+// use of spread operator is very usefull in concatination of arrays. 
+
+const arr = [1,2,3,[4,5],[6,[7,8]]]
+
+const newarr = arr.flat(Infinity);
+//console.log(newarr);
+
+
+//console.log(Array.from({name: "Adeeb"}));
+
+//Taking symbol as a object and printing it.
+
+const symb = Symbol("key")
+
+const obj = {
+    [symb] : "mykey"
+}
+
+//console.log(obj);
+
+
+//to Freeze the object Object.freeze(obj)
+
+// assign multiple obj into a target object 
+// const objtarget = Object.assign({},obj1,obj2,obj3)
+          //or
+// const obj3 = {...obj1,...obj2,...obj3}
+
